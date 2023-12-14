cbb4b7c7552277d8aa5a61c10447aa867346cb45
kaula <kay147839@gmail.com>
2023-12-14 12:01:00 +0800

    圖阿

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 FPS = 60
 WIDTH  = 500 
@@ -7,26 +7,101 @@ HEIGHT = 600
 
 WHITE = (255,255,255)
 GREEN = (0, 255, 0)
+GREY = (190,190,190)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+BLACK = (0,0,0)
 #遊戲初始化 創視窗
 pygame.init()
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("雞弊你")
 clock = pygame.time.Clock()
 
-class Player(pygame.sprite.Sprite):
+#俗投
+class Rock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,30))
+        self.image.fill((RED))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, WIDTH-self.rect.width)
+        self.rect.y = random.randrange(-100,-40)
+        self.speedy = random.randrange(2,10)
+        self.speedx = random.randrange(-5,4)
+        
+
+
+    def update(self):
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.top > HEIGHT or self.rect.left > WIDTH or self.rect.right < 0:
+            self.rect.x = random.randrange(0, WIDTH-self.rect.width)
+            self.rect.y = random.randrange(-100,-40)
+            self.speedy = random.randrange(2,10)
+            self.speedx = random.randrange(-5,4)
+     
+#飛船
+class Player1(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50,40))
         self.image.fill((GREEN))
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2,HEIGHT/2)
+        self.rect.centerx = WIDTH/2
+        self.rect.bottom = HEIGHT- 90
+        self.speedx = 10
+        self.speedy = 10
+
+
     def update(self):
-        self.rect.x 
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[pygame.K_RIGHT]:
+            self.rect.x += self.speedx
+        if key_pressed[pygame.K_LEFT]:
+            self.rect.x -= self.speedx
+        if key_pressed[pygame.K_UP]:
+            self.rect.y -= self.speedy
+        if key_pressed[pygame.K_DOWN]:
+            self.rect.y += self.speedy
+       
+
+
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+        if self.rect.right < 0:
+            self.rect.left = WIDTH 
+        if self.rect.top < 0:
+            self.rect.bottom = HEIGHT
+        if self.rect.bottom >HEIGHT:
+            self.rect.top = 0
+
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+#組但
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10,20))
+        self.image.fill((BLACK))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speedy = -10 
+
+    def update(self):
+       self.rect.y += self.speedy
+       if self.rect.bottom < 0:
+           self.kill() 
 
 all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
-
+player1 = Player1()
+all_sprites.add(player1)
+for i in range(8):
+    r = Rock()
+    all_sprites.add(r)
+    
 #迴圈
 running = True
 while running:
@@ -35,6 +110,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player1.shoot()
+
     #更新遊戲
     all_sprites.update()
     #畫面顯示
